0bb1298bd4d110554c11f0fe5acf17eb7274c533
Paolo Ardoino <paolo@tether.to>
2023-07-13 14:27:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const async = require('async')
+const _ = require('lodash')
+const RPC = require('@hyperswarm/rpc')
+const Base = require('bfx-facs-base')
+const { promisify } = require('util')
+
+const buildServer = async (conf) => {
+  const rpc = new RPC(conf)
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  return { rpc, server }
+}
+
+const buildClient = async (conf) => {
+  const rpc = new RPC()
+
+  const client = rpc.connect(conf.peer)
+
+  return { client, rpc }
+}
+
+class RpcFacility extends Base {
+  constructor (caller, opts, ctx) {
+    super(caller, opts, ctx)
+
+    this.name = 'rpc'
+    this.mode = opts.mode
+    this._hasConf = true
+
+    this.init()
+  }
+
+  _start (cb) {
+    async.series([
+      next => { super._start(next) },
+      async () => {
+        switch (this.mode) {
+          case 'server':
+            {
+              const built = await buildServer(_.pick(
+                this.conf,
+                ['seed']
+              ))
+
+              this.rpc = built.rpc
+              this.server = built.server
+            }
+            break
+          case 'client':
+            {
+              const built = await buildClient(_.pick(
+                this.conf,
+                ['peer']
+              ))
+
+              this.rpc = built.rpc
+              this.client = built.client
+            }
+            break
+          default:
+            throw new Error('ERR_MODE_UNDEFINED')
+        }
+      }
+    ], cb)
+  }
+
+  _stop (cb) {
+    async.series([
+      next => { super._stop(next) },
+      async () => {
+        switch (this.mode) {
+          case 'server':
+            await this.server.end()
+            break
+          case 'client':
+            await this.client.end()
+            break
+        }
+
+        await this.rpc.destroy()
+      }
+    ], cb)
+  }
+}
+
+module.exports = RpcFacility
